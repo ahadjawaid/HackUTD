48f6cd7641f58d8c7603b5ab1b4d975119ea319b
Kevin G <ge.kevin.a@gmail.com>
2022-11-13 09:18:30 -0600

    Add glassy effect to navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,4 +1,16 @@
-import { AppBar, Box, Button, ButtonBase, IconButton, ListItemIcon, ListItemText, Menu, MenuItem, Toolbar, Tooltip } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Button,
+  ButtonBase,
+  IconButton,
+  ListItemIcon,
+  ListItemText,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Tooltip,
+} from "@mui/material";
 import { Stack } from "@mui/system";
 import React from "react";
 import UserAvatar from "../components/UserAvatar";
@@ -31,47 +43,78 @@ class Navbar extends React.Component {
 
   render() {
     let openUserMenu = Boolean(this.state.toggleUserMenu);
-    let color = (this.props.type === "transparent") ? "transparent" : "background"
-    let elevation = (this.props.type === "transparent") ? 0 : 5
+    let color =
+      this.props.type === "transparent" ? "transparent" : "background";
+    let elevation = this.props.type === "transparent" ? 0 : 5;
     const bold = { fontWeight: 800 };
 
-    return <AppBar position="sticky" sx={{ padding: 1 }} color={color} elevation={elevation}>
-      <Toolbar>
-        <ButtonBase sx={bold} component="a" href={AuthService.authenticated ? "/dashboard" : "/"} disableRipple>
-          <Logo />
-        </ButtonBase>
+    return (
+      <AppBar
+        position="sticky"
+        sx={{
+          padding: 1,
+          boxSizing: "border-box",
+          top: 0,
+          backdropFilter: "blur(16px)",
+        }}
+        color={color}
+        elevation={elevation}
+      >
+        <Toolbar>
+          <ButtonBase
+            sx={bold}
+            component="a"
+            href={AuthService.authenticated ? "/dashboard" : "/"}
+            disableRipple
+          >
+            <Logo />
+          </ButtonBase>
 
-        <Box component="div" sx={{ flexGrow: 1 }} />
+          <Box component="div" sx={{ flexGrow: 1 }} />
 
-        <Stack direction="row" alignItems="center" spacing={1.2} component="div">
-          {AuthService.authenticated &&
-          <div>
-            <Tooltip title="Account settings">
-              <IconButton onClick={this.openUserMenu}>
-                <UserAvatar name={AuthService.user.name} />
-              </IconButton>
-            </Tooltip>
+          <Stack
+            direction="row"
+            alignItems="center"
+            spacing={1.2}
+            component="div"
+          >
+            {AuthService.authenticated && (
+              <div>
+                <Tooltip title="Account settings">
+                  <IconButton onClick={this.openUserMenu}>
+                    <UserAvatar name={AuthService.user.name} />
+                  </IconButton>
+                </Tooltip>
 
-            <Menu anchorEl={this.state.toggleUserMenu} open={openUserMenu} onClose={this.closeUserMenu}>
-              <MenuItem onClick={this.logout}>
-                <ListItemIcon>
-                  <Logout />
-                </ListItemIcon> 
-                <ListItemText primary="Sign Out" />
-              </MenuItem>
-            </Menu>
-          </div>
-          }
+                <Menu
+                  anchorEl={this.state.toggleUserMenu}
+                  open={openUserMenu}
+                  onClose={this.closeUserMenu}
+                >
+                  <MenuItem onClick={this.logout}>
+                    <ListItemIcon>
+                      <Logout />
+                    </ListItemIcon>
+                    <ListItemText primary="Sign Out" />
+                  </MenuItem>
+                </Menu>
+              </div>
+            )}
 
-          {!AuthService.authenticated &&
-            <Button sx={bold} href="/login" variant="contained">Sign In</Button>
-          }
-          {!AuthService.authenticated &&
-            <Button sx={bold} href="signup" variant="contained">Sign Up</Button>
-          }
-        </Stack>
-      </Toolbar>
-    </AppBar>;
+            {!AuthService.authenticated && (
+              <Button sx={bold} href="/login" variant="contained">
+                Sign In
+              </Button>
+            )}
+            {!AuthService.authenticated && (
+              <Button sx={bold} href="signup" variant="contained">
+                Sign Up
+              </Button>
+            )}
+          </Stack>
+        </Toolbar>
+      </AppBar>
+    );
   }
 }
 
